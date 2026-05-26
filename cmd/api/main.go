6654b35a234059981f08de28430f84e9e417f968
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-26 21:44:22 +0530

    feat: Load config and pool in main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,40 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/ayushWeb07/Notes-Rest-API/internal/config"
+	"github.com/ayushWeb07/Notes-Rest-API/internal/database"
+	"github.com/gin-gonic/gin"
+)
 
 func getHome(c *gin.Context) {
 	c.JSON(200, gin.H{
-		"message": "Working fine as always",
-		"status":  200,
+		"message":  "Working fine as always",
+		"database": "Connected",
+		"status":   200,
 	})
 }
 
 func main() {
+
+	// load the config
+	config, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// connect to database and get the pool
+	pool, err := database.ConnectWithDatabase(config.DatabaseConnectionUri)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// close the pool finally at the end
+	defer pool.Close()
+
 	// create a router instance
 	router := gin.Default()
 
@@ -17,5 +42,5 @@ func main() {
 	router.GET("/", getHome)
 
 	// run the server router
-	router.Run(":3000")
+	router.Run(":" + config.Port)
 }
