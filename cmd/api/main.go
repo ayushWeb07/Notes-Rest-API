9626378c1dfc1039445fec773e928b912ecc48e1
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-26 14:10:57 +0530

    feat: Create simple get home route

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
+
+func getHome(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Working fine as always",
+		"status":  200,
+	})
+}
 
 func main() {
-	fmt.Println("Starting the server...")
+	// create a router instance
+	router := gin.Default()
+
+	// setup routes
+	router.GET("/", getHome)
+
+	// run the server router
+	router.Run(":3000")
 }
