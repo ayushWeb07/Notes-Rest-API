ca890c791183b7851abeea621c8958333281983b
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-27 16:13:47 +0530

    feat: Add note go model

---
+++ internal/models/note.model.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Note struct {
+	ID          int       `json:"id" db:"id"`
+	Title       string    `json:"title" db:"title"`
+	Description string    `json:"description" db:"description"`
+	CreatedAt   time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at" db:"updated_at"`
+}
