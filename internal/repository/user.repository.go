1536833ff93408a769feb26fe2bf10d0e85451c4
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-28 12:02:37 +0530

    feat: Add repository for getting user by id and email

--- internal/repository/user.repository.go
+++ internal/repository/user.repository.go
@@ -35,3 +35,55 @@ func RegisterUser(pool *pgxpool.Pool, email string, password string) (*models.Us
 
 	return &newUser, nil
 }
+
+func GetUserByEmail(pool *pgxpool.Pool, email string) (*models.User, error) {
+	// create a timed context to free up resources associated with it & avoid slow connections
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+
+	// release resource once all the operations are done
+	defer cancelFunc()
+
+	// get single row query
+	query := "SELECT id, email, created_at, updated_at FROM users WHERE email=$1"
+	user := models.User{}
+
+	err := pool.QueryRow(ctx, query, email).Scan(
+		&user.ID,
+		&user.Email,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		fmt.Println("Something went wrong while fetching a user from database:", err)
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func GetUserById(pool *pgxpool.Pool, id int) (*models.User, error) {
+	// create a timed context to free up resources associated with it & avoid slow connections
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+
+	// release resource once all the operations are done
+	defer cancelFunc()
+
+	// get single row query
+	query := "SELECT id, email, created_at, updated_at FROM users WHERE id=$1"
+	user := models.User{}
+
+	err := pool.QueryRow(ctx, query, id).Scan(
+		&user.ID,
+		&user.Email,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		fmt.Println("Something went wrong while fetching a user from database:", err)
+		return nil, err
+	}
+
+	return &user, nil
+}
