66cbd978e401c7406b42e74d2f5d589070e5adf4
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-28 11:54:04 +0530

    feat: Add repository for registering user

---
+++ internal/repository/user.repository.go
@@ -0,0 +1,37 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/ayushWeb07/Notes-Rest-API/internal/models"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func RegisterUser(pool *pgxpool.Pool, email string, password string) (*models.User, error) {
+	// create a timed context to free up resources associated with it & avoid slow connections
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+
+	// release resource once all the operations are done
+	defer cancelFunc()
+
+	// insert query
+	newUser := models.User{}
+
+	query := "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id, email, created_at, updated_at"
+
+	err := pool.QueryRow(ctx, query, email, password).Scan(
+		&newUser.ID,
+		&newUser.Email,
+		&newUser.CreatedAt,
+		&newUser.UpdatedAt,
+	)
+
+	if err != nil {
+		fmt.Println("Something went wrong while registering user into database:", err)
+		return nil, err
+	}
+
+	return &newUser, nil
+}
